2b4b3f308fafdd143681db8841b95d867edb01fb
kalujja <kal-J>
2021-12-14 10:33:16 +0300

    declare global

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,8 +1,16 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === 'production') {
+declare global {
+  namespace NodeJS {
+    interface Global {
+      prisma: any;
+    }
+  }
+}
+
+if (process.env.NODE_ENV === "production") {
   prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
